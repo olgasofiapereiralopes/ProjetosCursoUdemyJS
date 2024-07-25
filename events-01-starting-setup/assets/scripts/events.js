b7f84840da112613f34e7b04cc7adf51c809d7bb
olgasofiapereiralopes <sophye_777_@hotmail.com>
2024-07-25 12:39:37 +0100

    newChanges

--- events-01-starting-setup/assets/scripts/events.js
+++ events-01-starting-setup/assets/scripts/events.js
@@ -1,20 +1,40 @@
-const button = document.querySelector('button');
+const buttons = document.querySelectorAll('button');
 
 // button.onclick = function () {
 
 // };
 
-// const buttonClickHandler = () => {
-//     alert('Button was clicked!');
-// };
+const buttonClickHandler = event => {
+    // event.target.disabled = true;
+    console.log(event);
+};
 
-// const anotherButtonClickHandler = () => {
-//     console.log('This was clicked!');
-// };
+const anotherButtonClickHandler = () => {
+    console.log('This was clicked!');
+};
 
 // button.onclick = buttonClickHandler;
 // button.onclick = anotherButtonClickHandler;
 
-button.addEventListener();
+const boundFn = buttonClickHandler.bind(this);
 
-// button.removeEventListener();
+// button.addEventListener('click', buttonClickHandler);
+
+// setTimeout(() => {
+//     button.removeEventListener('click', buttonClickHandler);
+// }, 2000);
+
+// buttons.forEach(btn => {
+//     btn.addEventListener('mouseenter', buttonClickHandler);
+// });
+
+// window.addEventListener('scroll', event => {
+//     console.log(event);
+// });
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    console.log(event);
+});
