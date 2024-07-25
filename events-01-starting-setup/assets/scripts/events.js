6ca4ede4ecc611df018dfab8abe192f0a9ba82dd
olgasofiapereiralopes <sophye_777_@hotmail.com>
2024-07-25 16:36:47 +0100

    newChanges25/07

--- events-01-starting-setup/assets/scripts/events.js
+++ events-01-starting-setup/assets/scripts/events.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('button');
+const button = document.querySelector('button');
 
 // button.onclick = function () {
 
@@ -37,4 +37,36 @@ const form = document.querySelector('form');
 form.addEventListener('submit', event => {
     event.preventDefault();
     console.log(event);
+});
+
+const div = document.querySelector('div');
+
+div.addEventListener('mouseenter', event => {
+    console.log('CLICKED DIV');
+    console.log(event);
+});
+
+button.addEventListener('click', function(event) {
+    event.stopPropagation();
+    console.log('CLICKED BUTTON');
+    console.log(event);
+    console.log(this);
+});
+
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// listItems.forEach(listItem => {
+//     listItem.addEventListener('click', event => {
+//         event.target.classList.toggle('highlight');
+//     });
+// });
+
+list.addEventListener('click', function(event) {
+    // console.log(event.currentTarget);
+    // event.target.classList.toggle('highlight');
+    event.target.closest('li').classList.toggle('highlight');
+    // form.submit();
+    button.click();
+    console.log(this);
 });
